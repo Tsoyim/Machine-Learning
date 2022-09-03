30758273f533b706deb04d8c01e48d419762bab6
Tsoyim <952390497@qq.com>
2022-09-03 20:54:51 +0800

    9-3

---
+++ DecisionTree/DecisionTree01.py
@@ -0,0 +1,27 @@
+# 决策树模型
+from pyexpat import features
+import numpy as np
+
+def generate_dataSet():
+    # 年龄（0青年、1中年、2老年） 工作（0无、1有） 房子（0无、1有）信贷情况（0一般、1好、2非常好）
+    dataSet = [[0, 0, 0, 0, 'no'],         #数据集
+        [0, 0, 0, 1, 'no'],
+        [0, 1, 0, 1, 'yes'],
+        [0, 1, 1, 0, 'yes'],
+        [0, 0, 0, 0, 'no'],
+        [1, 0, 0, 0, 'no'],
+        [1, 0, 0, 1, 'no'],
+        [1, 1, 1, 1, 'yes'],
+        [1, 0, 1, 2, 'yes'],
+        [1, 0, 1, 2, 'yes'],
+        [2, 0, 1, 2, 'yes'],
+        [2, 0, 1, 1, 'yes'],
+        [2, 1, 0, 1, 'yes'],
+        [2, 1, 0, 2, 'yes'],
+        [2, 0, 0, 0, 'no']]
+    features = ['年龄', '有工作', '有自己的房子', '信贷情况']	
+    return dataSet, features
+
+def calcEntropy(dataSet):
+    
+    
